d9ef6af5236103d080b71c56ca457160fa9bb745
Jordan Davis <jdavis.software@gmail.com>
2024-08-22 10:06:12 -0700

    Updates: @component(HomePage) - main component commit

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  effect,
+  ViewEncapsulation,
+  OnInit,
+} from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+import { DataTransformService } from './data-transform.service';
+import { Router } from '@angular/router'; // Import Router
+import { RecipeAPIService } from '../shared/services/recipe-api.service';
+import { NgIf, NgFor, CommonModule, AsyncPipe } from '@angular/common';
+import { ChipModule } from 'primeng/chip';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NgIf,
+    NgFor,
+    AsyncPipe,
+    ChipModule,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  private data = inject(DataTransformService);
+  private recipeAPI = inject(RecipeAPIService);
+  private router = inject(Router);
 
+  readonly categories$ = this.data.categories;
+  readonly recipeList$ = this.data.recipeList;
+
+  onHandleCategoryClick(categoryName: string) {
+    console.log(`category name: ${categoryName}`);
+    this.recipeAPI.selectedCategorySubject.next(categoryName);
+  }
+
+  onHandleRecipeClick(recipeId: number) {
+    console.log(`recipeId`, recipeId);
+    this.router.navigate([`details/${recipeId}`]); // Navigate to details page
+  }
 }
