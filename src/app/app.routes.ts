c419421df57c48dd89cc4c53b352f533aed85c2a
Jordan Davis <jdavis.software@gmail.com>
2024-08-22 09:50:59 -0700

    Updates: @Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    data: { breadcrumb: 'Home', icon: 'pi pi-home' },
+  },
+  {
+    path: 'details/:recipeId',
+    component: DetailPageComponent,
+    data: { breadcrumb: 'Details', icon: 'pi pi-home' },
+  },
+];
