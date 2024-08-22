ad7ff1d42104cded977d674a41e159b59a3a4175
Jordan Davis <jdavis.software@gmail.com>
2024-08-22 10:04:36 -0700

    Updates: @comonent(App)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { SearchComponent } from './shared/components/search/search.component';
+import { RecipeAPIService } from './shared/services/recipe-api.service';
+import { DataTransformService } from './home-page/data-transform.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent, CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'recipe-angular-app';
+  title = 'Intel 471:: Recipe Book';
+  private data = inject(DataTransformService);
+  private recipeAPI = inject(RecipeAPIService);
+
+  readonly searchResults$ = this.data.filteredRecipesByName;
+
+  handleSearch(searchValue: string): void {
+    console.log('Search query:', searchValue);
+    this.recipeAPI.searchRecipeByNameSubject.next(searchValue || '');
+  }
 }
